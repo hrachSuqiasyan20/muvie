1a0423e83341e69048f6e74b1a79fab1b22448c4
hrachSuqiasyan20 <suqiasyanhrach20@gmail.com>
2024-09-19 18:03:58 +0400

    Add files via upload

---
+++ src/components/header/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className=''>
+        <div className='back-fon'>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Home
